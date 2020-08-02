421e5cd499ca272ed1b8991da3dd89a10d8b42ab
Pascal Betting <pascal.betting@gmx.de>
2020-08-02 22:07:26 +0200

    deps: update to latest nx; add nrwl angular package for testing

--- libs/ddd/jest.config.js
+++ libs/ddd/jest.config.js
@@ -2,8 +2,9 @@ module.exports = {
   name: 'ddd',
   preset: '../../jest.config.js',
   transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest'
+    '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../coverage/libs/ddd'
+  coverageDirectory: '../../coverage/libs/ddd',
+  globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
 };
