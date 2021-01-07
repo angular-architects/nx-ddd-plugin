fb6a9e0d7257ab176eca8d16b7439167e9fdb008
Peter Smith <peter@upstateinteractive.io>
2021-01-06 19:42:00 -0500

    feat(schematics): include @ngrx/schematics in ng add command
    Assuming a user of the plugin is using it in a project with @ngrx/schematics then there is no benefit.
    If a user has not installed @ngrx/schematics then this will protect them from a "collection not found" error.

--- libs/ddd/src/schematics/ng-add/index.ts
+++ libs/ddd/src/schematics/ng-add/index.ts
@@ -1,11 +1,16 @@
-import { 
-  chain, 
-  Rule, 
+import {
+  chain,
+  Rule
 } from '@angular-devkit/schematics';
+import { addDepsToPackageJson } from '@nrwl/workspace';
 import { initLintingRules } from '../rules';
+import { NGRX_VERSION } from '../utils';
 
 export default function(): Rule {
   return chain([
     initLintingRules(),
+    addDepsToPackageJson({
+      '@ngrx/schematics': NGRX_VERSION
+    })
   ]);
 }
