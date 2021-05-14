2dd74d48681cbbe66da5601d47593c7eae0bd3f3
wizardnet972 <wizardnet972@gmail.com>
2021-05-14 23:17:43 +0300

    fix(schematics): add domainDirectory to the import path in feature component
    add domainDirectory to the import path in feature component
    
    ISSUES CLOSED: #62

--- libs/ddd/src/schematics/feature/files/forFeature/__name@dasherize__.component.ts
+++ libs/ddd/src/schematics/feature/files/forFeature/__name@dasherize__.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit} from '@angular/core';
-import { <%=classify(name)%>Facade } from '<%=workspaceName%>/<%=dasherize(domain)%>/domain';
+import { <%=classify(name)%>Facade } from '<%=workspaceName%>/<%=dasherize(domain)%><%- domainDirectory ? '/' + domainDirectory : '' %>/domain';
 
 @Component({
   selector: '<%=dasherize(domain)%>-<%=dasherize(name)%>',
